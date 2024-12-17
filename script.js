a75e8b631d30fbb544553d872fe68e5599b5b687
Krauzer94 <13894059+Krauzer94@users.noreply.github.com>
2024-12-17 19:07:49 -0300

    Constructor function for the Book objects

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(author, title, pages, status) {
+  this.bookAuthor = author;
+  this.bookTitle = title;
+  this.bookPages = pages;
+  this.bookStatus = status; // 'Read' or 'Not read'
 }
 
 function addBookToLibrary() {
