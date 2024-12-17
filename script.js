04364bfc261e9997fccff0aee00ba37bcd6250c3
Krauzer94 <13894059+Krauzer94@users.noreply.github.com>
2024-12-17 19:27:18 -0300

    Add first book using the previously created method

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
+// Store all Book created objects
 const myLibrary = [];
 
 // Constructor for the Book object
 function Book(author, title, pages, status) {
-  this.bookAuthor = author;
-  this.bookTitle = title;
-  this.bookPages = pages;
-  this.bookStatus = status; // 'Read' or 'Not read'
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.status = status; // 'Read' or 'Not read'
 }
 
 // Add Book objects to the library
-function addBookToLibrary() {
+function addBookToLibrary(author, title, pages, status) {
   const newBook = new Book(author, title, pages, status);
   myLibrary.push(newBook);
 }
+
+// The Handbook
+addBookToLibrary("Epictetus", "The Handbook", 123, true);
+console.log(myLibrary[0]);
