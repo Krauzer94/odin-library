fe01f8bc86dbfa54bde27dc331c91414582cdfa6
Krauzer94 <13894059+Krauzer94@users.noreply.github.com>
2024-12-18 20:59:05 -0300

    Add books using JavaScript with existing data

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const myLibrary = [];
 
 // Constructor for the Book object
 function Book(author, title, pages, status) {
+  // this.id = id; Automatically generated using the index
   this.author = author;
   this.title = title;
   this.pages = pages;
@@ -28,14 +29,46 @@ function displayMyLibrary(myLibraryArray) {
     console.log(`[#${[([i + 1])]}]: ${myLibraryArray[i]}`);
   };
 
-  // TODO: Display added books into page elements
-  // WIP...
+  // Main book shelf display element
+  const libraryShelf = document.querySelector('.library-shelf');
+  
+  // Create and inject book elements
+  myLibraryArray.forEach(book => {
+    // Main book div to add each book
+    const libraryBook = document.createElement('div')
+    libraryBook.classList.add('library-book');
+
+    // Create the book title element
+    const libraryBookTitle = document.createElement('p');
+    libraryBookTitle.textContent = `Title: ${book.title}`;
+
+    // Create the book author element
+    const libraryBookAuthor = document.createElement('p');
+    libraryBookAuthor.textContent = `Author: ${book.author}`;
+
+    // Create the book pages element
+    const libraryBookPages = document.createElement('p');
+    libraryBookPages.textContent = `Pages: ${book.pages}`;
+
+    // Create the book read status
+    const libraryBookStatus = document.createElement('p');
+    libraryBookStatus.textContent = `Status: ${book.status ? 'Read' : 'Not read'}`;
+
+    // Add created elements into the page
+    libraryBook.appendChild(libraryBookTitle);
+    libraryBook.appendChild(libraryBookAuthor);
+    libraryBook.appendChild(libraryBookPages);
+    libraryBook.appendChild(libraryBookStatus);
+
+    // Add created book into the main shelf
+    libraryShelf.appendChild(libraryBook);
+  });
 }
 
 // Manually add a couple of books
 addBookToLibrary("Epictetus", "The Handbook", 123, true);
 addBookToLibrary("Marcus Aurelius", "Meditations", 231, true);
-addBookToLibrary("Neal Stephenson", "Snow Crash", 440, false);
+// addBookToLibrary("Neal Stephenson", "Snow Crash", 440, false);
 
 // Manually display the added books
 displayMyLibrary(myLibrary);
